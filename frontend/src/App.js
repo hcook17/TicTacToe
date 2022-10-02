23cfc7f1a222e329a2609d91f7a685e714c39caa
hcook17 <hcook6@emich.edu>
2022-10-02 00:06:50 -0400

    TicTacToe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data";
+import {useEffect, useState} from 'react';
 
-function App() {
+export default function App() {
+  const [board, setBoard] = useState(data.pieces);
+  const [turn, setTurn] = useState("X");
+  const [freeze, setFreeze] = useState(false);
+  const [winner, setWinner] = useState('');
+
+  function clickHandler(props) {
+    if(board[props].symbol !== '-') {
+      return;
+    }
+
+    const updatePiece = {
+      id: props,
+      symbol: turn
+    }
+
+    board[props] = updatePiece;
+    setBoard([...board]);
+    checkWin(board);
+
+    if(turn === 'X') {
+  
+      setTurn('O');
+      setFreeze(true);
+      setTimeout(() => computerMove(board), 1500);
+    } 
+  
+  }
+
+  function computerMove(props) {
+    const availableSpots = props.filter((piece) => piece.symbol === '-');
+    if(availableSpots.length === 0) {
+      return;
+    }
+    availableSpots[0].symbol = "O";
+    setBoard([...board])
+    checkWin(board);
+    setTurn('X');
+    setFreeze(false);
+  }
+
+  function checkWin(props) {
+    const board = props;
+    console.log(board);
+    if(board[0].symbol == 'X' && board[3].symbol == 'X' && board[6].symbol == 'X') {
+      setWinner('X');
+      setFreeze(true);
+    } else if (board[1].symbol === 'X' && board[4].symbol === 'X' && board[7].symbol === 'X') {
+      setWinner('X');
+      setFreeze(true);
+    } else if (board[2].symbol === 'X' && board[5].symbol === 'X' && board[8].symbol === 'X') {
+      setWinner('X');
+      setFreeze(true);
+    } else if (board[0].symbol === 'X' && board[1].symbol === 'X' && board[2].symbol === 'X') {
+      setWinner('X');
+      setFreeze(true);
+    } else if (board[3].symbol === 'X' && board[4].symbol === 'X' && board[5].symbol === 'X') {
+      setWinner('X');
+      setFreeze(true);
+    } else if (board[6].symbol === 'X' && board[7].symbol === 'X' && board[8].symbol === 'X') {
+      setWinner('X');
+      setFreeze(true);
+    } else if (board[0].symbol === 'X' && board[4].symbol === 'X' && board[8].symbol === 'X') {
+      setWinner('X');
+      setFreeze(true);
+    } else if (board[2].symbol === 'X' && board[4].symbol === 'X' && board[6].symbol === 'X') {
+      setWinner('X');
+      setFreeze(true);
+    }
+    if (board[0].symbol == 'O' && board[3].symbol == 'O' && board[6].symbol == 'O') {
+      setWinner('O');
+      setFreeze(true);
+    } else if (board[1].symbol === 'O' && board[4].symbol === 'O' && board[7].symbol === 'O') {
+      setWinner('O');
+      setFreeze(true);
+    } else if (board[2].symbol === 'O' && board[5].symbol === 'O' && board[8].symbol === 'O') {
+      setWinner('O');
+      setFreeze(true);
+    } else if (board[0].symbol === 'O' && board[1].symbol === 'O' && board[2].symbol === 'O') {
+      setWinner('O');
+      setFreeze(true);
+    } else if (board[3].symbol === 'O' && board[4].symbol === 'O' && board[5].symbol === 'O') {
+      setWinner('O');
+      setFreeze(true);
+    } else if (board[6].symbol === 'O' && board[7].symbol === 'O' && board[8].symbol === 'O') {
+      setWinner('O');
+      setFreeze(true);
+    } else if (board[0].symbol === 'O' && board[4].symbol === 'O' && board[8].symbol === 'O') {
+      setWinner('O');
+      setFreeze(true);
+    } else if (board[2].symbol === 'O' && board[4].symbol === 'O' && board[6].symbol === 'O') {
+      setWinner('O');
+      setFreeze(true);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <>
+     <h1>R u smart? ...probably not ;)</h1>
+      <h2>Turn: {turn}</h2>
+      {
+        winner &&
+      <h2>
+        Winner: {winner}
+      </h2>
+      }
+      <div>
+      {board.map((piece) => (
+        <button key={piece.id} disabled={freeze} onClick={() => clickHandler(piece.id)}> {piece.symbol} </button>
+      ))}
+      </div>
+    </>
+  )};
+  
+     
---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    pieces: [
+        { 
+            id: 0,
+            symbol: "-"
+        }, 
+        { 
+            id: 1, 
+            symbol: "-"
+        },
+        { 
+            id: 2,
+            symbol: "-" 
+        },
+        { 
+            id: 3,
+            symbol: "-" 
+        }, 
+        { 
+            id: 4,
+            symbol: "-" 
+        }, 
+        { 
+            id: 5,
+            symbol: "-" 
+        },
+        { 
+            id: 6,
+            symbol: "-" 
+        },
+        { 
+            id: 7,
+            symbol: "-" 
+        },
+        { 
+            id: 8,
+            symbol: "-" 
+        },
+        ],
+};
+
+export default data;
